1c334bd0f0e26127b3abc6e7ccf014964d56aee2
Muhamad Zainul Kamal <zainul.intern.pasti@pegadaian.co.id>
2024-10-06 23:47:51 +0700

    Generic Method

--- src/learn/java/generic/application/GenericApp.java
+++ src/learn/java/generic/application/GenericApp.java
@@ -2,17 +2,22 @@ package learn.java.generic.application;
 
 import learn.java.generic.MyData;
 import learn.java.generic.Pair;
+import learn.java.generic.util.ArrayHelper;
 
 public class GenericApp {
     public static void main(String[] args) {
         MyData<String> stringMyData = new MyData<String>("Ajay");
         MyData<Integer> integerMyData = new MyData<Integer>(100);
-
         System.out.println(stringMyData.getData());
         System.out.println(integerMyData.getData());
 
         Pair<String, Integer> pair = new Pair<String, Integer>("Ajay", 100);
         System.out.println(pair.getFirst());
         System.out.println(pair.getSecond());
+
+        String[] names = {"Muhamad", "Zainul", "Kamal"};
+        Integer[] numbers = {1,2,3,4,5,};
+        System.out.println(ArrayHelper.count(names));
+        System.out.println(ArrayHelper.count(numbers));
     }
 }
